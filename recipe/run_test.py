26653d8887d62d21958997aaaa4755253f02a88b
Nicholas Bollweg <nick.bollweg@gmail.com>
2026-09-25 16:13:46 -0500

    more abi3 test

--- recipe/run_test.py
+++ recipe/run_test.py
@@ -5,11 +5,11 @@ import os
 from typing import Any
 import json
 
-PYTHON_MIN = os.environ["python_min"]
-SRC_DIR = Path(os.environ["SRC_DIR"])
+PYTHON_MIN = os.environ["PYTHON_MIN"]
 SP_DIR = Path(os.environ["SP_DIR"])
-IS_ABI3 = json.loads(os.environ["is_abi3"])
-ABI3_EXT = "pyd" if os.name == "nt" else "pyo3.so"
+
+IS_ABI3 = bool(json.loads(os.environ["IS_ABI3"].lower()))
+ABI3_EXT = "pyd" if os.name == "nt" else "abi3.so"
 
 
 UNLINK_TESTS = [
@@ -17,18 +17,20 @@ UNLINK_TESTS = [
     "crates/jsonschema-py/tests-py/test_annotation_suite.py",
 ]
 
-COMMENT_TESTS = {
-    # needs unpackaged jsonschema_testsuite_pyo3
-    "crates/jsonschema-py/tests-py/test_suite.py": [
-        ("import jsonschema_testsuite_pyo3", "#"),
-    ]
+NO_JS_TEST_PYO3 = ("import jsonschema_testsuite_pyo3", "# not packaged")
+
+PATCH_TESTS = {
+    # needs unpackaged jsonschema_testsuite_pyo3 (#67)
+    "crates/jsonschema-py/tests-py/test_suite.py": [NO_JS_TEST_PYO3],
+    "crates/jsonschema-py/tests-py/conftest.py": [NO_JS_TEST_PYO3]
 }
 
 SKIPS = [
     # not relevant
     "readme",
-    # needs jsonschema_testsuite_pyo3
-    "test_draft_codegen",
+    # needs jsonschema_testsuite_pyo3 (#67)
+    "codegen",
+    "concurrent_mutation_does_not_crash",
 ]
 
 PYTEST_ARGS = [
@@ -37,20 +39,20 @@ PYTEST_ARGS = [
     "--tb=long",
     "--color=yes",
     "crates/jsonschema-py",
-    "-k"
+    "-k",
     f"""not ({" or ".join(SKIPS)})""",
 ]
 
 
 def unlink_tests() -> int:
     for name in UNLINK_TESTS:
-        (SRC_DIR / name).unlink()
+        Path(name).unlink()
     return 0
 
 
-def comment_tests() -> int:
-    for name, patterns in COMMENT_TESTS.items():
-        path = Path(SRC_DIR / name)
+def patch_tests() -> int:
+    for name, patterns in PATCH_TESTS.items():
+        path = Path(name)
         text = path.read_text(encoding="utf-8")
         for find, replace in patterns:
             text = text.replace(find, replace)
@@ -68,15 +70,16 @@ def maybe_abi3audit() -> int:
         "-v",
         "--assume-minimum-abi3",
         PYTHON_MIN,
-        *SP_DIR.glob(f"jsonschemea_rs/**/*.{ABI3_EXT}"),
+        *SP_DIR.glob(f"jsonschema_rs/**/*.{ABI3_EXT}"),
     )
 
 
 def do(*args: Any) -> int:
     str_args = list(map(str, args))
-    print(">>>", *str_args)
+    print(">>>", *str_args, flush=True)
     return call(str_args)
 
 
 if __name__ == "__main__":
-    sys.exit(unlink_tests() or comment_tests() or do(*PYTEST_ARGS) or maybe_abi3audit())
+    print(f"PYTHON_MIN: {PYTHON_MIN}  IS_ABI3: {IS_ABI3}  ABI3_EXT: {ABI3_EXT}")
+    sys.exit(unlink_tests() or patch_tests() or do(*PYTEST_ARGS) or maybe_abi3audit())
